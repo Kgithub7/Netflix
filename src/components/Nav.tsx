b907ab21dccdf897e49939083bf57838262d4928
Kobi <knjoku@hawk.illinoistech.edu>
2025-10-14 19:06:20 -0500

    Complete navigation bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,37 @@
+import Logo from "../assets/logo.png";
+import SearchIcon from "../assets/search_icon.svg";
+import BellIcon from "../assets/bell_icon.svg";
+import ProfileImg from "../assets/profile_img.png";
+import DropdownIcon from "../assets/caret_icon.svg";
+
 const Nav = () => {
-  return <></>;
+  return (
+    <div className="fixed flex w-full justify-between bg-linear-180 from-black to-transparent px-[6%] py-5 text-sm text-[#e5e5e5]">
+      <div className="flex items-center gap-x-12.5">
+        <img src={Logo} className="w-23" />
+        <ul className="flex gap-x-5 [&>*]:cursor-pointer">
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="flex items-center gap-x-5 [&>img]:w-5 [&>img]:cursor-pointer">
+        <img src={SearchIcon} />
+        <p>Children</p>
+        <img src={BellIcon} />
+        <div className="relative flex cursor-pointer items-center gap-x-2.5 hover:[&>div]:block">
+          <img src={ProfileImg} className="w-9 rounded-sm" />
+          <img src={DropdownIcon} />
+          <div className="absolute top-full right-0 z-1 hidden w-max rounded-xs bg-[#191919] px-5.5 py-4.5 underline [&>p]:cursor-pointer [&>p]:text-sm">
+            <p>Sign Out of Netflix</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
